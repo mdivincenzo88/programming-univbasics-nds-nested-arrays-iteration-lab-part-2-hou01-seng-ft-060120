fd0b848767c29c49befb0aad79de1e5de43e2fa2
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-21 06:03:11 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,9 +8,10 @@ while row_index < src.count do
   element_index = 0
   while element_index < src[row_index].count do
     if src[row_index][element_index] < min_value
-      min value = src[row_index][element_index] 
+      min_value = src[row_index][element_index] 
      
     end
+    new_array << min_value
     element_index += 1
   end
   row_index += 1
